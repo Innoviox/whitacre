79d5e6693a88da696d33998d90012a8f71aba3f3
Simon Chervenak <sarras305@gmail.com>
2022-09-01 12:51:14 -0400

    tile displayss

--- tetris.py
+++ tetris.py
@@ -15,6 +15,7 @@ class Tile:
     x: int
     y: int
     shape: str
+    color: str
 
 class Board:
     def __init__(self):
@@ -33,36 +34,44 @@ class Board:
                 self.board[-1].append('B')
 
     def display(self):
-        for row in self.board:
-            for sq in row:
-                print(sq, end='')
-            print()
+        disp = [i[:] for i in self.board[:]]
+        if self.tile:
+            print(self.tile)
+            for idx, length in enumerate(self.tile.shape):
+                print(idx, length)
+                y = self.tile.y + idx
+                if y < len(disp):
+                    for x_add in range(int(length)):
+                        disp[len(disp) - y - 1][self.tile.x + x_add] = self.tile.color
+
+        print('\n'.join(''.join(i) for i in disp))
 
     def start_game(self):
         while not self.full():
+            self.display()
             if self.tile is None:
                 self.drop_tetris()
-            # self.take_input()
+            self.take_input()
             self.tick()
 
     def drop_tetris(self):
         shape = random.choice(TILES)
 
-        self.tile = Tile(0, self.rows, shape)
+        self.tile = Tile(0, self.rows, shape, random.choice(COLORS))
 
     def full(self):
         ...
 
     def take_input(self):
-        ...
+        input()
         # todo rotate
 
     def tick(self):
         self.tile.y -= 1
-        if self.tile.y == 0:
-            self.fix_tile_to_board()
+        if self.tile.y == 0: # it can hit a tile on the board, todo
+            # self.fix_tile_to_board()
             self.tile = None
         
 
 b = Board()
-b.display()
+b.start_game()
